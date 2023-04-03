90d9ea2c4269c4e2c972deffff87c4d1d70cfddf
jonatanare <jonatanare.dev@gmail.com>
2023-04-03 10:55:03 -0500

    delete escript analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,40 +33,6 @@
 
     <link rel="stylesheet" href="{{asset('css/custom-styles.css')}}">
 
-    @if(env('APP_ENV') == "production" )
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FRYN8E5TR4"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-FRYN8E5TR4');
-    </script>
-
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/61fd7ecd9bd1f31184db04de/1g14htb54';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
-    @else
-
-    @endif
-
-
     @stack('styles')
 
 </head>
